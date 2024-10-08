54791bddc2e839c03420cd9fa42455427e0ec42b
Igor Videira <igor-luiz94@hotmail.com>
2024-10-08 18:01:15 -0300

    Primeiro Model

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list= new List<Department>();
+            return View(list);
+        }
+    }
+}
