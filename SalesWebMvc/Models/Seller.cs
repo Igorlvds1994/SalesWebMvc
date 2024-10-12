89605cdc83d67960c6f6015bf0486fa84fccf43c
Igor Videira <igor-luiz94@hotmail.com>
2024-10-12 15:14:00 -0300

    SellerFormViewModel and Department

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,6 +8,7 @@
         public double BaseSalary { get; set; }
         public DateTime BirthDate { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales{ get; set; } = new List<SalesRecord>();
         public Seller()
         {
@@ -19,7 +20,7 @@
             Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
-            Department = department;
+            
 
         }
         public void AddSales(SalesRecord sr) 
